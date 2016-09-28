e96fdef8973626fd8757eb485616dadc1baaa394
Amit Chambial <chambialamit@gmail.com>
2016-09-28 19:05:33 +0530

    @#$.py

---
+++ Amit/5.py
@@ -0,0 +1,29 @@
+list1=[1,"abcd",2.5,4321]
+list2=[1,"my name is john",45.23]
+print(list1)
+print(list2)
+print(list1[0])
+print(list1[1:4])
+print(list2[0])
+print(list2[1:4])
+print(list1+list2)
+tuple1=(1,"deven",2.5)
+tuple2=(2,"amit",3.5)
+print(tuple1)
+print(tuple2)
+print(tuple1[0])
+print(tuple2[0])
+print(tuple1[1:2])
+print(tuple2[0:1])
+print(tuple1+tuple2)
+
+dict={}
+dict['one']="Hello a"
+dict[2]="Hello b"
+tiny={'he':'she',1:'one',2:'two'}
+print(dict)
+print(dict['one'])
+print(dict[2])
+print(tiny)
+print(tiny.keys())
+print(tiny.values())
